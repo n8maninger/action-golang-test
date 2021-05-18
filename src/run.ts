2ff1338037cf7f100b9d98ab0145f6aa24aa1bf3
Nate <me@n8m.us>
2021-05-19 08:06:20 +0900

    remove unnecessary var

--- src/run.ts
+++ src/run.ts
@@ -29,33 +29,30 @@ function process(line: string) {
 		if (!results)
 			results = { elapsed: 0, output: [] };
 
-		let elapsed;
 		switch (parsed.Action) {
 		case 'output':
 			results.output.push(parsed.Output);
 			break;
 		case 'fail':
-			elapsed = parseFloatOrDefault(parsed.Elapsed);
 			totalRun++;
+			results.elapsed = parseFloatOrDefault(parsed.Elapsed);
 			failed.push(key);
-			results.elapsed = elapsed;
 
-			if (optLongRunningTestDuration !== -1 && elapsed >= optLongRunningTestDuration)
-				core.info(`\u001b[33m${key} took ${elapsed}s to fail`);
+			if (optLongRunningTestDuration !== -1 && results.elapsed >= optLongRunningTestDuration)
+				core.info(`\u001b[33m${key} took ${results.elapsed}s to fail`);
 
 			if (!optShowStdOut)
-				core.info(`\u001b[31m${key} failed in ${elapsed}s`);
+				core.info(`\u001b[31m${key} failed in ${results.elapsed}s`);
 			break;
 		case 'pass':
-			elapsed = parseFloatOrDefault(parsed.Elapsed);
 			totalRun++;
-			results.elapsed = elapsed;
+			results.elapsed = parseFloatOrDefault(parsed.Elapsed);
 
-			if (optLongRunningTestDuration !== -1 && elapsed >= optLongRunningTestDuration)
-				core.info(`\u001b[33m${key} took ${elapsed}s to pass`);
+			if (optLongRunningTestDuration !== -1 && results.elapsed >= optLongRunningTestDuration)
+				core.info(`\u001b[33m${key} took ${results.elapsed}s to pass`);
 
 			if (!optShowStdOut && optShowPassedTests)
-				core.info(`\u001b[32m${key} passed in ${elapsed}s`);
+				core.info(`\u001b[32m${key} passed in ${results.elapsed}s`);
 			break;
 		}
 
